96221ea285d19efd04d5962bafe5f69d8fbc73d9
ErviePogi <erviejohn.villareal79@gmail.com>
2025-12-09 00:10:32 +0800

    Initial gh-pages commit

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Projects.css';
+
+function Projects() {
+  return (
+    <div className="projects">
+        <div className="card">
+            <h4>Design of Anomaly Detection
+                System of Household Electricity
+                Consumption - 2022
+            </h4>
+        </div>
+
+        <div className="card">
+            <h4>Detection of Blood Quantity using
+                Image Processing - 2021
+            </h4>
+        </div>
+
+        <div className="card">
+            <h4>A Management Tool Software for
+                RL Land Registration Services with
+                Database Management System -
+                2021
+            </h4>
+        </div>
+
+        <div className="card">
+            <h4>Cyber Attacks During the
+                Pandemic and Awareness of
+                Filipino about Cybersecurity,
+                Technological Institute of the
+                Philippines Quezon City - 2021
+            </h4>
+        </div>
+
+        <div className="card">
+            <h4>Application of the Analytic
+                Hierarchy Process to Improve the
+                Mental Health of Computer
+                Engineering Students in T.I.P.
+                Quezon City - 2020
+            </h4>
+        </div>
+
+        <div className="card">
+            <h4>Interpersonal Relationship’s
+                Influence to Senior High School
+                Toward Academic Competency -
+                2018
+            </h4>
+        </div>
+
+    </div>
+  )
+}
+
+export default Projects
